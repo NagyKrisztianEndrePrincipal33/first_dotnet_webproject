de73a0135acbc3922a43e203562693cf1277d3c3
AzureAD\KrisztianNagy <krisztian.nagy@principal33.com>
2021-09-01 12:36:30 +0300

    Creating convertResponseToWeatherForecast function

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using RestSharp;
 
 namespace AspNetSandbox.Controllers
 {
@@ -26,6 +27,28 @@ namespace AspNetSandbox.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
+
+            var client = new RestClient("https://api.openweathermap.org/data/2.5/onecall?lat=46.652010&lon=24.484990&exclude=hourly,minutely&appid=5c308e24cae79e04481631a88fa4acc5");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.GET);
+            IRestResponse response = client.Execute(request);
+            Console.WriteLine(response.Content);
+
+            return ConvertResponseToWeatherForecast(response.Content);
+
+            //var rng = new random();
+            //return enumerable.range(1, 5).select(index => new weatherforecast
+            //{
+            //    date = datetime.now.adddays(index),
+            //    temperaturec = rng.next(-20, 55),
+            //    summary = summaries[rng.next(summaries.length)]
+            //})
+            //.toarray();
+        }
+
+        private IEnumerable<WeatherForecast> ConvertResponseToWeatherForecast(string content)
+        {
+
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
@@ -33,7 +56,9 @@ namespace AspNetSandbox.Controllers
                 TemperatureC = rng.Next(-20, 55),
                 Summary = Summaries[rng.Next(Summaries.Length)]
             })
-            .ToArray();
+
+                .ToArray();
+
         }
     }
 }
