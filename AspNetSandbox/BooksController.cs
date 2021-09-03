6ffb5efcb78e37ab0715de8db72cc89b1062ded9
AzureAD\KrisztianNagy <krisztian.nagy@principal33.com>
2021-09-03 10:38:49 +0300

    Adding a book model and controller and adding the first book

---
+++ AspNetSandbox/BooksController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AspNetSandbox
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+
+        private Book[] books;
+        public BooksController()
+        {
+            books = new Book[2];
+            books[0] = new Book();
+            books[0].Id = 1;
+            books[0].Title = "Magic Book";
+            books[0].Language = "Spanish";
+            books[0].Author = "Paolo Coelho";
+        }
+        // GET: api/<BooksController>
+        [HttpGet]
+        public IEnumerable<Book> Get()
+        {
+            return  books;
+        }
+
+        // GET api/<BooksController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<BooksController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<BooksController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<BooksController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
