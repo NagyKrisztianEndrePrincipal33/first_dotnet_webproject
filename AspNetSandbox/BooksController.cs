da3b8bf5dbc9e5c90d60aa052ed9ad9590db51b8
AzureAD\KrisztianNagy <krisztian.nagy@principal33.com>
2021-09-03 10:41:33 +0300

    Use object initialization.

--- AspNetSandbox/BooksController.cs
+++ AspNetSandbox/BooksController.cs
@@ -17,17 +17,21 @@ namespace AspNetSandbox
         public BooksController()
         {
             books = new Book[2];
-            books[0] = new Book();
-            books[0].Id = 1;
-            books[0].Title = "Magic Book";
-            books[0].Language = "Spanish";
-            books[0].Author = "Paolo Coelho";
+            books[0] = new Book
+            {
+                Id = 1,
+                Title = "Magic Book",
+                Language = "Spanish",
+                Author = "Paolo Coelho"
+            };
 
-            books[1] = new Book();
-            books[1].Id = 2;
-            books[1].Title = "The Witcher";
-            books[1].Language = "Hungarian";
-            books[1].Author = "Andrzej Sapkowski";
+            books[1] = new Book
+            {
+                Id = 2,
+                Title = "The Witcher",
+                Language = "Hungarian",
+                Author = "Andrzej Sapkowski"
+            };
         }
         // GET: api/<BooksController>
         [HttpGet]
