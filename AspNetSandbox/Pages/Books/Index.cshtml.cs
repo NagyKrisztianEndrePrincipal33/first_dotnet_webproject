e7ee0db93853caa23f9a920a41dde0b0cd225b32
AzureAD\KrisztianNagy <krisztian.nagy@principal33.com>
2021-09-10 08:16:48 +0300

    Create Razor Pages for CRUD operations

---
+++ AspNetSandbox/Pages/Books/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using AspNetSandbox.Data;
+using AspNetSandbox.Models;
+
+namespace AspNetSandbox.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly AspNetSandbox.Data.ApplicationDbContext _context;
+
+        public IndexModel(AspNetSandbox.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Book = await _context.Book.ToListAsync();
+        }
+    }
+}
