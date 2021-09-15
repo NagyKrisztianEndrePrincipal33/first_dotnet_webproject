06cdc415a8e94012b5f13901b11c4bf92fa4456a
NagyKrisztianEndrePrincipal33 <87873704+NagyKrisztianEndrePrincipal33@users.noreply.github.com>
2021-09-15 12:29:28 +0300

    Appending the table when a new book was added

--- AspNetSandbox/wwwroot/js/books.js
+++ AspNetSandbox/wwwroot/js/books.js
@@ -5,7 +5,22 @@ var connection = new signalR.HubConnectionBuilder().withUrl("/messagehub").build
 // Disable send button until connection is establishes
 
 connection.on("BookCreated", function (book) {
-    console.log("Book created: "+book.toString());
+    $("tbody").append(`<tr>
+        <td>
+            ${book.title}
+        </td>
+        <td>
+            ${book.author}
+        </td>
+        <td>
+            ${book.language}
+        </td>
+        <td>
+            <a href="/Books/Edit?id=${book.id}">Edit</a> |
+            <a href="/Books/Details?id=${book.id}">Details</a> |
+            <a href="/Books/Delete?id=${book.id}">Delete</a>
+        </td>
+    </tr>`);
 });
 
 connection.start().then(function () {
