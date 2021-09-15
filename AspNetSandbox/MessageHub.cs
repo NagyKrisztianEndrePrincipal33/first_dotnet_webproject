4988fa089b0f1cb6943c73f6517ead3e7f3c480a
NagyKrisztianEndrePrincipal33 <87873704+NagyKrisztianEndrePrincipal33@users.noreply.github.com>
2021-09-15 10:51:40 +0300

    Fix some warning

--- AspNetSandbox/MessageHub.cs
+++ AspNetSandbox/MessageHub.cs
@@ -2,14 +2,14 @@
 // Copyright (c) PlaceholderCompany. All rights reserved.
 // </copyright>
 
-using Microsoft.AspNetCore.SignalR;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
 
 namespace AspNetSandbox
 {
     public class MessageHub : Hub
     {
-        public async Task SendMessage(string user ,string message)
+        public async Task SendMessage(string user, string message)
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
         }
